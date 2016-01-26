a58d6d21b741541b3a6f7682955f789aba8219c6
Anton Pawlik <anton.pawlik@gmail.com>
2016-01-26 14:45:37 +0100

    linting

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ function isIp(req, safeIps) {
   var clientIp = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.ip;
   return clientIp && safeIps && safeIps.indexOf(clientIp) !== -1;
 }
+
 function resetCache(res) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
   res.header('Pragma', 'no-cache');
 }
+
 function isMapFile(url, scopeDir) {
   if (scopeDir && !(new RegExp('^'+scopeDir).test(url))) {
     return;
   }
   return url.indexOf('.js.map') !== -1 || url.indexOf('.css.map') !== -1;
-};
+}
 
 module.exports = function(safeIps, scopeDir) {
   return [
